862abb6d5e554eb437f2892dc31cd4ce8af9670c
k-yaoi <kaoru.yaoi@gmail.com>
2018-05-06 08:07:58 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  #一対多を表現
+  has_many :microposts
 end
